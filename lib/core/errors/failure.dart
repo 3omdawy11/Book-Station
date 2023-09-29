2915c51b4391d1e314fea82d03c5ccb10665c18c
3omdawy11 <mohamedzaky970@gmail.com>
2023-09-29 23:45:44 +0300

    created featured box state using cubit

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,1 +1,60 @@
-class Failure {}
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  String message;
+
+  Failure(this.message);
+}
+class ServerFailure extends Failure{
+  ServerFailure(super.message);
+  factory ServerFailure.fromDioError(DioException error) {
+    String errorMessage;
+
+    switch (error.type) {
+      case DioExceptionType.connectionTimeout:
+        errorMessage = "Connection timeout";
+        break;
+      case DioExceptionType.receiveTimeout:
+        errorMessage = "Receive timeout";
+        break;
+      case DioExceptionType.sendTimeout:
+        errorMessage = "Send timeout";
+        break;
+      case DioExceptionType.badResponse:
+        errorMessage = ServerFailure.fromResponse(statusCode: error.response!.statusCode!, response: error.response!.data) as String;
+        break;
+      case DioExceptionType.cancel:
+        errorMessage = "Request cancelled";
+        break;
+      case DioExceptionType.badCertificate:
+        errorMessage = "Bad Certificate";
+        break;
+      case DioExceptionType.connectionError:
+        errorMessage = "Connection Error";
+        break;
+      default:
+        errorMessage = "Please Try Again Later";
+        break;
+    }
+
+    return ServerFailure(errorMessage);
+  }
+  factory ServerFailure.fromResponse({required int statusCode, required dynamic response}){
+    if (statusCode == 404){
+      return ServerFailure('Your request was not found, please try later');
+    }else if (statusCode == 500) {
+      return ServerFailure('There is a problem with server, please try later');
+    }else if (statusCode == 400 || statusCode == 401 || statusCode == 403){
+      return ServerFailure(response['error']['message']);
+    }
+    return ServerFailure('There was an error please try again');
+  }
+}
+
+
+class CacheFailure extends Failure{
+  CacheFailure(super.message);
+}
+class NetworkFailure extends Failure{
+  NetworkFailure(super.message);
+}
--- lib/feature/home_page/domain/repo/home_repo_implementation.dart
+++ lib/feature/home_page/domain/repo/home_repo_implementation.dart
@@ -3,7 +3,7 @@ import 'package:book_station/feature/home_page/data/data_sources/home_remote_dat
 import 'package:book_station/feature/home_page/domain/entities/book_entity.dart';
 import 'package:book_station/feature/home_page/domain/repo/home_repo.dart';
 import 'package:dartz/dartz.dart';
-
+import 'package:dio/dio.dart';
 import '../../data/data_sources/home_local_data_source.dart';
 
 class HomeRepoImplementation extends HomeRepo {
@@ -15,28 +15,35 @@ class HomeRepoImplementation extends HomeRepo {
   @override
   Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
     try {
-      var cachedBooks = homeLocalDataSource.fetchFeaturedBooks();
-      if (cachedBooks.isNotEmpty) {
-        return right(cachedBooks); // return if there is cached books
+      List<BookEntity> books;
+      books = homeLocalDataSource.fetchFeaturedBooks();
+      if (books.isNotEmpty) {
+        return right(books); // return if there is cached books
       }
-      var remoteBooks = await homeRemoteDataSource.fetchFeaturedBooks();
-      return right(remoteBooks);
+      books =
+          await homeRemoteDataSource.fetchFeaturedBooks(); // get remote Books
+      return right(books);
     } catch (e) {
-      return left(Failure());
+      return (e is DioError)
+          ? left(ServerFailure.fromDioError(e))
+          : left(ServerFailure(e.toString()));
     }
   }
 
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async{
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
     try {
-      var cachedBooks = homeLocalDataSource.fetchNewestBooks();
-      if (cachedBooks.isNotEmpty) {
-        return right(cachedBooks); // return if there is cached books
+      List<BookEntity> books;
+      books = homeLocalDataSource.fetchNewestBooks();
+      if (books.isNotEmpty) {
+        return right(books); // return if there is cached books
       }
-      var remoteBooks = await homeRemoteDataSource.fetchNewestBooks();
-      return right(remoteBooks);
+      books = await homeRemoteDataSource.fetchNewestBooks(); // get remote Books
+      return right(books);
     } catch (e) {
-      return left(Failure());
+      return (e is DioError)
+          ? left(ServerFailure.fromDioError(e))
+          : left(ServerFailure(e.toString()));
     }
   }
 }
